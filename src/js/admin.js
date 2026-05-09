439a316f3123437d97bc150ec9440e6ae2c88dbd
rahimihesam <hesamrahimiwork@gmail.com>
2026-05-09 11:28:39 +0330

    Add popups.

--- src/js/admin.js
+++ src/js/admin.js
@@ -5,3 +5,19 @@ function openModal(id){
 function closeModal(id){
     document.getElementById(id).style.display="none"
 }
+
+let alertTimeout = 5000;
+
+// تابع برای بستن آلرت
+function closeAlert() {
+    let alertElement = document.getElementById('myAlert');
+    if (alertElement) {
+        alertElement.classList.remove('show');
+        setTimeout(function() {
+            alertElement.style.display = 'none';
+        }, 500);
+    }
+}
+
+// اجرای تابع پس از گذشت زمان مشخص شده
+setTimeout(closeAlert, alertTimeout);
