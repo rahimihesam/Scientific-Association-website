099c249d9f05c16b7912df6487e156c3f05657e2
rahimihesam <hesamrahimiwork@gmail.com>
2026-05-15 18:32:33 +0330

    Fixed Bugs.

--- src/js/admin.js
+++ src/js/admin.js
@@ -21,3 +21,18 @@ function closeAlert() {
 
 // اجرای تابع پس از گذشت زمان مشخص شده
 setTimeout(closeAlert, alertTimeout);
+
+
+// Little Show and Hide Password
+const passwordInput = document.getElementById('password');
+const toggleButton = document.getElementById('toggle-admin-view');
+
+toggleButton.addEventListener('click', function() {
+    const currentType = passwordInput.getAttribute('type');
+
+    if (currentType === 'password') {
+        passwordInput.setAttribute('type', 'text');
+    } else {
+        passwordInput.setAttribute('type', 'password');
+    }
+});
